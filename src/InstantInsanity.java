8af518e8c38b98256eaf4340a5ddc4bc733d7b96
MikeM <mikejon0829@gmail.com>
2018-11-15 19:20:22 -0800

    Algorithm change for generating subsets.

--- src/InstantInsanity.java
+++ src/InstantInsanity.java
@@ -1,13 +1,11 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 
 public class InstantInsanity {
-	static Map<Integer, ColorCube> colorCubes = new HashMap<Integer, ColorCube>();
+	static ArrayList<Integer> colorCubes = new ArrayList<Integer>();
 	private static Queue<Integer> thread = new LinkedList<Integer>();
 	
 	private static final int numOfCubeFaces = 3;
@@ -18,52 +16,42 @@ public class InstantInsanity {
 		ColorCube cube2 = new ColorCube(new int []{0,1}, new int []{1,2}, new int []{3,5});
 		ColorCube cube3 = new ColorCube(new int []{0,1}, new int []{1,2}, new int []{3,5});
 		
-		colorCubes.put(0, cube1);
-		colorCubes.put(1, cube2);
-		colorCubes.put(2, cube3);
+		colorCubes.add(0);
+		colorCubes.add(1);
+		colorCubes.add(2);
 		
 		List<Boolean> hasSolution = new ArrayList<Boolean>();
-		boolean [] cubeRows = new boolean [colorCubes.size()];
-		do {
-			hasSolution.add(checkCubesForConflict(cubeRows));
-		}while(nextSubset(cubeRows));
+		/*
+		for(int i = 1; i < colorCubes.length(); i++) {
+			int num, k;
+			k = num = num | (i << 1);
+			for(int j = i; j < colorCubes.length - i; j++) {
+				k = k & 3 << (i + j)
+			}
+		}*/
+
+		for(int i = 2; i < colorCubes.length; i++) {
+			for(int j = 0; j < i; j++) {
+				int notThisIndex = (Math.pow(2, i) - 1) - Math.pow(2, j);
+				hasSolution.add(dfsCubes(notThisIndex, i));
+			}
+		}
 		
 		System.out.println(hasSolution);
 	}
-	//updates cubeRows with our selected subset. Only the true values are valid
-	private static  boolean nextSubset(boolean [] cubeRows) {
-		int i = 0;
-		while(i < cubeRows.length && cubeRows[i]) {
-			cubeRows[i] = false;
-			i++;
-		}
-		if ( i == cubeRows.length) {
-			return false;
-		}
-		
-		cubeRows[i] = true;
-		
-		return true;
-	}
+
 	
-	
-	private static boolean checkCubesForConflict(boolean [] cubeRows) {
-		//retrieve keys of the cubes being used e.g. [2,4,5,8] -> work with cubes 2,4,5,8 only
-		List<Integer> cubeIndices = getCubeIndices(cubeRows);
-		return dfsCubes(cubeIndices, 0);
-	}
-	
-	public static boolean dfsCubes(List<Integer> cubeIndices, int count) {
-		if(thread.size() == cubeIndices.size()) {
+	public static boolean dfsCubes(int voidIndex, int subsetSize) {
+		if(thread.size() == subsetSize) {
 			count++;
 			//save thread optional
 			//Debug thread here
 			return true;
 		}
-		for(int j = 0; j < 3; j++) {
-			thread.add(j);
-			if(solutionHasNoConflict(cubeIndices))
-				dfsCubes(cubeIndices, count);
+		for(int i = 0; i < subsetSize && i != voidIndex; i++) {
+			thread.add(i);
+			if(solutionHasNoConflict(voidIndex, subsetSize))
+				dfsCubes(voidIndex, subsetSize);
 			thread.remove();
 		}
 			
@@ -73,7 +61,7 @@ public class InstantInsanity {
 		return false;
 	}
 	
-	private static boolean solutionHasNoConflict(List<Integer> cubeIndices) {
+	private static boolean solutionHasNoConflict(int voidIndex, int subsetSize) {
 		
 		//return true if next pair matches previous
 		if(thread.size() > 1) {
@@ -81,23 +69,11 @@ public class InstantInsanity {
 			//thread contains a 0,1,or 2 which represents left-right, front-back, top-bottom
 			int oppPair1 = it.next();
 			int oppPair2 = it.next();
-			return colorCubes.get(
-					//get selected indices instead of contiguous indices
-					//size of thread tells us where to look within our cubeIndices table
-					cubeIndices.get(thread.size() - 1)).getColorFaces()[oppPair1]
-					.equals(colorCubes.get(cubeIndices.get(thread.size() - 2)).getColorFaces()[oppPair2]);
+			return colorCubes[thread.size() - 1].getColorFaces()[oppPair1]
+					.equals(colorCubes[thread.size() - 2)].getColorFaces()[oppPair2]);
 		}
 			
 		return true;
-	}
-	
-	private static List<Integer> getCubeIndices(boolean [] cubeRows) {
-		List<Integer> cubeIndices = new ArrayList<Integer>();
-		for(int i =0; i < cubeRows.length; i++) {
-			if(cubeRows[i] == true)
-				cubeIndices.add(i);
-		}
-		return cubeIndices;
 	}
 
 }
